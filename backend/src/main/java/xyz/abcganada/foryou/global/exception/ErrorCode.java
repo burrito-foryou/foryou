02ecc324a5896cce7df4f393181d7c770f5a5b8a
강현민 <sasara4@naver.com>
2026-06-29 12:43:41 +0900

    feat: WBS0303 WBS0309 질문 작성 API 및 조건 태그 입력 처리 완료

--- backend/src/main/java/xyz/abcganada/foryou/global/exception/ErrorCode.java
+++ backend/src/main/java/xyz/abcganada/foryou/global/exception/ErrorCode.java
@@ -29,7 +29,11 @@ public enum ErrorCode {
     //answer
     ANSWER_NOT_FOUND(HttpStatus.NOT_FOUND, "ANSWER_001", "답변을 찾을 수 없습니다."),
     ANSWER_FORBIDDEN(HttpStatus.FORBIDDEN, "ANSWER_002", "답변 작성자만 수정/삭제할 수 있습니다."),
-    ANSWER_ALREADY_ACCEPTED(HttpStatus.BAD_REQUEST, "ANSWER_003", "이미 채택된 질문입니다.");
+    ANSWER_ALREADY_ACCEPTED(HttpStatus.BAD_REQUEST, "ANSWER_003", "이미 채택된 질문입니다."),
+
+    // question
+    QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "QUESTION_001", "질문을 찾을 수 없습니다."),
+    QUESTION_FORBIDDEN(HttpStatus.FORBIDDEN, "QUESTION_002", "질문 작성자만 수정/삭제할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
